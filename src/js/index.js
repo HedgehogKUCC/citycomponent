5783572b87cd3a78d299735f82bff4c5e5fe2724
HedgehogKU <ccc908925@gmail.com>
2020-02-27 11:08:06 +0800

    完成取出不重複的城市

--- src/js/index.js
+++ src/js/index.js
@@ -22,6 +22,19 @@ const app = new Vue({
       let res = await fetch(api)
       let data = await res.json()
       vm.data = data
+
+      // 取出所有城市
+      let allCity = []
+      vm.data.forEach(item => allCity.push(item.County))
+
+      // console.log(allCity);
+
+      // 參考 https://guahsu.io/2017/06/JavaScript-Duplicates-Array/
+      // 取出不重複的城市
+      vm.location = allCity.filter((e, i, a) => a.indexOf(e) === i )
+
+      // console.log(vm.location);
+
     }
   },
   created() {
